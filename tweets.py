221098ede8483e48024b638ca92309717de85736
frozhusain <45816083+frozhusain@users.noreply.github.com>
2019-01-04 01:14:15 +0530

    added comments

--- tweets.py
+++ tweets.py
@@ -2,21 +2,24 @@ import tweepy
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
-
+#input #MeToo to get tweets about it
 hashtag_phrase=input("enter the keyword to search tweets::")
-ckey="cO4PJdfaFSvX25BB7ysQio7Q2"
-csecret="31REzY4Q8NRZuqQxlwCtBcgZZeqZyLiClxEyAoerhX6M91TfUb"
-atoken="2476790046-ueMP4B4lfaxkNgKpcqcdxlFAW5ZpTQuXE3dEfX1"
-asecret="iA1pKvofwVpbzhgtUyGQY9glulVn8mqzryoPRbIk0Csbk"
+#add your own keys
+ckey="           "
+csecret="             "
+atoken="          "
+asecret="     "
 
 auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
 ob = tweepy.API(auth)
 p=1
+#extract only 100 at once in english language 
 for tweet in tweepy.Cursor(ob.search,hashtag_phrase, lang="en", tweet_mode='extended').items(100):
     print(tweet.full_text.replace('\n',' ').encode('utf-8'))
     print('\n')
     print(p)
+#every time it will append tweets to a file     
     s=tweet.full_text.replace('\n',' ').encode('utf-8')
     saveFile=open('tweetextracted3.txt','a')
     saveFile.write(str(p))
